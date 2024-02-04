607fb3b59f83885f61f78fccaa64149c996598df
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-04 23:26:25 +0330

    analysis method needed : IMinimumWindowSubstring

---
+++ IMinimumWindowSubstring.cs
@@ -0,0 +1,8 @@
+﻿namespace Minimum_Window_Substring
+{
+    internal interface IMinimumWindowSubstring
+    {
+        List<string> Substrings(int lenght);
+        string MinWindowSubstring(List<string> substrings);
+    }
+}
