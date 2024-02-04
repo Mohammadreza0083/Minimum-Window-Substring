d88d6789cc62b5faa9e204a3e03a391795a68fb6
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-05 00:32:13 +0330

    Implementation MinWindowSubstring method

--- MinimumWindowSubstring.cs
+++ MinimumWindowSubstring.cs
@@ -1,32 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Minimum_Window_Substring
+﻿namespace Minimum_Window_Substring
 {
     public class MinimumWindowSubstring : IMinimumWindowSubstring
     {
-        public List<string> Substrings(int lenght, string input)
+        public string MinWindowSubstring(string s, string t)
         {
-            int chunckSize = lenght + 1;
-            List<string> substrings = new List<string>();
-            for (int i = 0; i < lenght; i++)
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t))
+                return "";
+
+            var tChars = new Queue<char>(t);
+            int required = tChars.Count;
+            int minStart = 0, minEnd = int.MaxValue;
+
+            for (int start = 0, end = 0; end < s.Length; end++)
             {
-                substrings.Add(input[i..(i + lenght)]);
+                var c = s[end];
+                if (tChars.Contains(c))
+                {
+                    tChars.Dequeue();
+                    required--;
+                }
+
+                while (required == 0)
+                {
+                    if (end - start < minEnd - minStart)
+                    {
+                        minStart = start;
+                        minEnd = end;
+                    }
+
+                    var firstChar = s[start];
+                    start++;
+
+                    if (tChars.Contains(firstChar))
+                    {
+                        tChars.Enqueue(firstChar);
+                        required++;
+                    }
+                }
             }
-            for (int i = 0; i < chunckSize; i++)
-            {
-                substrings.Add(input[i..(i + chunckSize)]);
-            }
-            return substrings;
+
+            return minEnd == int.MaxValue ? "" : s.Substring(minStart, minEnd - minStart + 1);
         }
-       
-        public string MinWindowSubstring(List<string> substrings)
-        {
-            throw new NotImplementedException();
-        }
+
 
     }
 }
