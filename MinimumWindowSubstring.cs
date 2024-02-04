d53349adc18a75cc81ae6336ef06f3040a583e69
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-04 23:51:17 +0330

    Implementation Substrings method

---
+++ MinimumWindowSubstring.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Window_Substring
+{
+    public class MinimumWindowSubstring : IMinimumWindowSubstring
+    {
+        public List<string> Substrings(int lenght, string input)
+        {
+            int chunckSize = lenght + 1;
+            List<string> substrings = new List<string>();
+            for (int i = 0; i < lenght; i++)
+            {
+                substrings.Add(input[i..(i + lenght)]);
+            }
+            for (int i = 0; i < chunckSize; i++)
+            {
+                substrings.Add(input[i..(i + chunckSize)]);
+            }
+            return substrings;
+        }
+       
+        public string MinWindowSubstring(List<string> substrings)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
