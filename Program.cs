307707b16690929796eebb0e492a3c6be82bc363
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-05 00:44:30 +0330

    test faild

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            MinimumWindowSubstring minimumWindowSubstring = new();
+            Console.WriteLine("Entre your first string : ");
+            string FirstInput = Console.ReadLine();
+            Console.WriteLine("Enter your second string : ");
+            string LastInput = Console.ReadLine();  
+            string minSub = minimumWindowSubstring.MinWindowSubstring(FirstInput, LastInput);
+            Console.WriteLine(minSub);
         }
     }
 }
