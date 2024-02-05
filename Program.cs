919e8ee579cc405eaa5ade543f8c96d4b984871e
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-05 20:52:58 +0330

    Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@
             Console.WriteLine("Entre your first string : ");
             string FirstInput = Console.ReadLine();
             Console.WriteLine("Enter your second string : ");
-            string LastInput = Console.ReadLine();  
-            string minSub = minimumWindowSubstring.MinWindowSubstring(FirstInput, LastInput);
-            Console.WriteLine(minSub);
+            string LastInput = Console.ReadLine();
+            string result =minimumWindowSubstring.MinWindowSubstring(FirstInput, LastInput);
+            Console.WriteLine($"Minimum window substring of {FirstInput} and {LastInput} is : {result}");
         }
     }
 }
